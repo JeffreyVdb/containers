44bd721c04dc62bd28ccb099d0c0053ca9e367ce
Jeffrey Vandenborne <vandenborne.jeffrey@gmail.com>
2018-01-23 09:49:37 +0100

    Always execute as www-data user

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+set -o errexit
+
+DAEMON_USER=www-data
+if [ $(id -u) -eq 0 ]; then
+    echo "Changing user to $DAEMON_USER"
+    su-exec "$DAEMON_USER" test -w "$APP_ROOT" || chown -R -- "$DAEMON_USER" "$APP_ROOT"
+    exec su-exec "$DAEMOM_USER" "$0" "$@"
+fi
 
 exec "$@"
